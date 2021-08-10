96dada5fb8cd2df84e1f9b457089faeb797d1f9c
niharpawade@outlook.com <niharpawade@outlook.com>
2021-08-10 20:45:45 +0530

    Initial commit

---
+++ src/test/java/com/example/EmployeManagementSyatem/EmployeManagementSyatemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EmployeManagementSyatem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeManagementSyatemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
